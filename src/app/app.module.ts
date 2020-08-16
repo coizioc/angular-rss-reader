73a3f00a51e207a75fa663b72e1f94f079818514
coizioc <36041795+coizioc@users.noreply.github.com>
2020-08-16 15:29:04 -0400

    Add Angular Material components to module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RssFeedComponent } from './rss-feed/rss-feed.component';
 import { RssItemComponent } from './rss-item/rss-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [
@@ -17,6 +20,9 @@ import { RssItemComponent } from './rss-item/rss-item.component';
         BrowserModule,
         HttpClientModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatToolbarModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/rss-item/rss-item.component.ts
+++ src/app/rss-item/rss-item.component.ts
@@ -8,10 +8,15 @@ import { Item } from '../interfaces';
 })
 export class RssItemComponent implements OnInit {
     @Input() item: Item;
+    imageSrc: string | undefined = undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+      if(this.item['media:content'] !== undefined) {
+          this.imageSrc = this.item['media:content'][0]['$'].url;
+      } else if(this.item['media:thumbnail'] !== undefined) {
+          this.imageSrc = this.item['media:thumbnail'];
+      }
   }
-
 }
